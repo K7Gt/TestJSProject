34e28887890d0dce73f8eb7352fc3c8c7bab993f
Andrey <gojiem777@gmail.com>
2021-11-02 22:05:52 +0200

    Added 1st task

--- js/script.js
+++ js/script.js
@@ -1,9 +1,25 @@
 "use strict";
 
-const category = 'toys';
+// const category = 'toys';
 
-console.log(`https://someurl.com/${category}/5`);
+// console.log(`https://someurl.com/${category}/5`);
 
-const user = 'Ivan';
+// const user = 'Ivan';
 
-alert(`Hello, ${user}`);
+// alert(`Hello, ${user}`);
+
+const numberOfFilms = +prompt("How many fimlms have you seen?");
+
+const personaMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},  
+    genres: [],
+    privat: false
+};
+const lastSeenFilm = prompt("Last seen film?",""),
+      lastSeenFilmRating = prompt("Rate this film","");
+
+personaMovieDB.movies[lastSeenFilm] = lastSeenFilmRating;
+
+console.log(personaMovieDB);
