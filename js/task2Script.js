cfe2ab53d4948bbbe36bedcf9d25c3a49fc38073
Andrey <gojiem777@gmail.com>
2021-11-04 22:29:30 +0200

    Added task3

--- js/task2Script.js
+++ js/task2Script.js
@@ -16,7 +16,17 @@
 
 // Код возьмите из предыдущего домашнего задания
 
-const numberOfFilms = +prompt("How many films have you seen?");
+let numberOfFilms;
+
+function start() {
+    numberOfFilms = +prompt("How many films have you seen?");
+
+    while(numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt("How many films have you seen?");
+    }
+}
+
+start();
 
 const personaMovieDB = {
     count: numberOfFilms,
@@ -26,25 +36,59 @@ const personaMovieDB = {
     privat: false
 };
 
-let count = 1;
+function rememberMyFilms(){
+    let count = 1;
 
-do{
-    let lastSeenFilm = prompt("Last seen film?",""),
-      lastSeenFilmRating = prompt("Rate this film","");
-      if((lastSeenFilm != null && lastSeenFilm.length < 50 && lastSeenFilm != '') && (lastSeenFilmRating != null && lastSeenFilmRating.length < 50 && lastSeenFilmRating != '')){
-        personaMovieDB.movies[lastSeenFilm] = lastSeenFilmRating;
-        count++;
-      }
-}while(count <=2);
-
-if (personaMovieDB.count < 10){
-    console.log("Просмотрено довольно мало фильмов");
-}else if(personaMovieDB.count >= 10 && personaMovieDB.count < 30){
-    console.log("Вы классический зритель");
-}else {
-    console.log("Вы киноман");
+    do{
+        let lastSeenFilm = prompt("Last seen film?",""),
+        lastSeenFilmRating = prompt("Rate this film","");
+        if((lastSeenFilm != null && lastSeenFilm.length < 50 && lastSeenFilm != '') && (lastSeenFilmRating != null && lastSeenFilmRating.length < 50 && lastSeenFilmRating != '')){
+            personaMovieDB.movies[lastSeenFilm] = lastSeenFilmRating;
+            count++;
+        }
+    }while(count <=2);
 }
 
-console.log(personaMovieDB);
+rememberMyFilms();
+
+
+
+function detectPersonalLevel(){
+    if (personaMovieDB.count < 10){
+        console.log("Просмотрено довольно мало фильмов");
+    }else if(personaMovieDB.count >= 10 && personaMovieDB.count < 30){
+        console.log("Вы классический зритель");
+    }else {
+        console.log("Вы киноман");
+    }
+}
+
+detectPersonalLevel();
+
+function showMyDB(){
+    if(!personaMovieDB.privat){
+        console.log(personaMovieDB);
+    }
+}
+
+
+function writeYourGenres(){
+    let genres = [];
+
+    let favoriteGenres;
+    let count = 0;
+    while(count <3) {
+        favoriteGenres = prompt(`Your favorite genre by the number ${count+1}?`);
+        genres.push(favoriteGenres);
+        count++;
+    }
+
+    personaMovieDB.genres = genres;
+}
+
+writeYourGenres();
+
+showMyDB();
+
 
 
